704a2c260fc7ce344f5f8732c01601bc62e5126e
Sokhib <127226435+smash3-11@users.noreply.github.com>
2023-05-12 19:08:46 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let btn14 = document.querySelector('#btn14')
+let btn14Max = document.querySelector('#btn14Max')
+let blac = document.querySelector('.blac')
+let img = document.querySelector('#img')
+btn14.onclick = ()=>{
+    btn14.style.border = '3px solid #0071e3'
+    blac.classList.toggle('none')
+    btn14Max.style.border = '1px solid gray'
+    blac.classList.add('none')
+    img.src = 'img/deeppurple.jpg'
+}   
+
+btn14Max.onclick = ()=>{
+    btn14Max.style.border = '3px solid #0071e3'
+    blac.classList.toggle('none')
+    btn14.style.border = '1px solid gray'
+    img.src="img/deeppurple.jpg"
+    blac.classList.add('none')
+
+}
+let color1 = document.querySelector('#color1')
+let color2 = document.querySelector('#color2')
+let color3 = document.querySelector('#color3')
+let color4 = document.querySelector('#color4')
+
+
+let color = document.querySelector('#color')
+
+color1.onmouseenter = ()=>{
+    color.innerHTML=' Purple'
+}
+color1.onclick = ()=>{
+    img.src = 'img/deeppurple.jpg'
+}
+color2.onmouseenter = ()=>{
+    color.innerHTML=' Gold'
+    
+}
+color2.onclick = ()=>{
+    img.src = 'img/golg.jpg'
+}
+color3.onmouseenter = ()=>{
+    color.innerHTML=' Silver'
+}
+color3.onclick = ()=>{
+    img.src = 'img/silver.jpg'
+}
+color4.onmouseenter = ()=>{
+    color.innerHTML=' Black'
+}
+color4.onclick = ()=>{
+    img.src = 'img/black.jpg'
+}
